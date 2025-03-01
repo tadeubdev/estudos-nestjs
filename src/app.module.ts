e28df79954c391d32f34a54508b007c3904244cb
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-01 12:48:58 -0300

    feat: adiciona suporte ao MongoDB com @nestjs/mongoose e mongoose no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    RoutesModule,
+    MongooseModule.forRoot(
+      'mongodb://root:root@db/projeto_teste?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
